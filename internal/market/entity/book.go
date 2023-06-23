221c9e16daa3cb0a618da870a7173cd41ae22714
Ester Alves <esteralves.s@icloud.com>
2023-06-23 14:52:27 -0300

    -

--- internal/market/entity/book.go
+++ internal/market/entity/book.go
@@ -80,23 +80,24 @@ func (b *Book) AddTransaction(transaction *Transaction, wg *sync.WaitGroup) {
 	}
 
 	transaction.SellingOrder.Investor.UpdateAssetPosition(transaction.SellingOrder.Asset.ID, -minShares)
-	transaction.SellingOrder.PendingShares -= minShares
+	transaction.AddSellOrderPendingShares(-minShares)
+	// transaction.SellingOrder.PendingShares -= minShares
 	transaction.BuyingOrder.Investor.UpdateAssetPosition(transaction.BuyingOrder.Asset.ID, minShares)
-	transaction.BuyingOrder.PendingShares -= minShares
+	transaction.AddBuyOrderPendingShares(-minShares)
+	// transaction.BuyingOrder.PendingShares -= minShares
 
 	//	transaction.Total = float64(transaction.Shares) * transaction.BuyingOrder.Price
-
 	transaction.CalculateTotal(transaction.Shares, transaction.BuyingOrder.Price)
 
 	//if transaction.BuyingOrder.PendingShares == 0 {
 	// 	transaction.BuyingOrder.Status = "CLOSED"
 	// }
-	// if transaction.SellingOrder.PendingShares == 0 {
-	// 	transaction.SellingOrder.Status = "CLOSED"
+	//if transaction.SellingOrder.PendingShares == 0 {
+	//	transaction.SellingOrder.Status = "CLOSED"
 	// }
 
-	transaction.CloseBuyTransaction()
-	transaction.CloseSellTransaction()
+	transaction.CloseBuyOrder()
+	transaction.CloseSellOrder()
 
 	b.Transactions = append(b.Transactions, transaction)
 }
